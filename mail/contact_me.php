a0122ffbed8344b0b8276628f5e13c7512c331f4
Kowaio <guillaume.siorak@gmail.com>
2020-02-21 17:08:03 +0100

    wip draft EVA

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -9,18 +9,18 @@ if(empty($_POST['name'])      ||
    echo "No arguments Provided!";
    return false;
    }
-   
+
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
-   
+
 // Create the email and send the message
-$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";   
+$to = 'europevilleurbanneaikido@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Formulaire de contact - Site internet :  $name";
+$email_body = "Vous avez reçu une nouveau message depuis votre formulaire de contact.\n\n"."Voici les détails :\n\Nom: $name\n\nE-mail: $email_address\n\nTél.: $phone\n\nMessage:\n$message";
+$headers = "From: noreply@aikidovilleurbanne.fr\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
-return true;         
+return true;
 ?>
